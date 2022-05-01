b2577ee916bdba1175123dc65e9c22bb728dd4c9
FiachraCI <fiachraholland@gmail.com>
2022-05-01 21:43:55 +0000

    Removed .txt from Procfile

--- esc_outlet/settings.py
+++ esc_outlet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,11 +103,15 @@ WSGI_APPLICATION = 'esc_outlet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://obkcnpxszjpcch:aa32d47c1d7aa3a0e156c4987f756d1c7113ddc73c8227b93ca76949f6a385ec@ec2-3-223-213-207.compute-1.amazonaws.com:5432/daukfm54bme51k')
 }
 
 
